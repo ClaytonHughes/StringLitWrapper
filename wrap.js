4888e532c0ec8acaeb988578addc2fc96e74f801
Clayton Hughes <clayton.hughes@gmail.com>
2013-02-12 02:06:27 -0800

    Output something meaningful for CLI mode

--- wrap.js
+++ wrap.js
@@ -1,21 +1,19 @@
-// Print args:
 var args = process.argv.slice(2);
-console.log('args: ', args);
 
 // Try to load library?
 wrapper = require('./wrapper.js');
 _ = require('./node_modules/underscore/underscore-min.js');
-
 fs = require('fs');
 
 _.each(args, function(arg) {
     fs.readFile(arg, function(err,data) {
         if(err) {
-            return console.log(err);
+            console.log("Error Reading "+arg +" : " + err);
         } else {
+            console.log("Wrapping " + arg + " -> " + arg + ".wrapped");
             var output = wrapper.wrapText(data.toString(), 78);
             fs.writeFile(arg + ".wrapped", output, function(err) {
-                console.log(err);
+                if(err) { console.log("Error Writing "+arg + " : " + err); }
             });
         }
     });
